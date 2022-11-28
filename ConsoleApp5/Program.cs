59078c3ac4e032a172424e6e3ab5addd3699a555
IlonaBekoeva <beklona30@gmail.com>
2022-11-28 11:08:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = { 5, 8, 1, 9, 78 };
+            foreach (var item in mas)
+            {
+                Console.WriteLine(item);    
+            }
+        }
+    }
+}
